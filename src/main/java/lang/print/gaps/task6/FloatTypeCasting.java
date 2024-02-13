3d631388dd53dffd50f476d4a514da485914c85d
Marcos <104943037+MarcosSSA011@users.noreply.github.com>
2024-02-12 23:10:53 -0600

    m3 t6 1

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,14 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
-    public void roundNumber(float numberToBeRounded) {
+    public void roundNumber(float numberToBeRounded) {    	    	    
+    	int roundedNumber = Math.round(numberToBeRounded);
+        System.out.println("Rounded number: " + roundedNumber);
+    }
+    
+    public static void main(String[] args) {
+        FloatTypeCasting obj = new FloatTypeCasting();
+        float numberToBeRounded = 5.67f; 
+        obj.roundNumber(numberToBeRounded);
     }
 }
